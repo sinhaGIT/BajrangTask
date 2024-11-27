c64436902d1e9fcd6a88c66484545b29b26a7996
Bajrang Sinha <sinhabajrangkgp@gmail.com>
2024-11-27 18:54:45 +0530

    Project initialized

---
+++ BajrangTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BajrangTask
+//
+//  Created by Bajrang Sinha on 27/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
